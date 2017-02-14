7b53341bcd0a9bd87c6570ca5bd42f0fa10e6cf4
koyexes <no-reply@email.com>
2017-02-14 14:04:06 +0100

    Initial commit

--- app/tests/dojo_tests.py
+++ app/tests/dojo_tests.py
@@ -3,6 +3,8 @@ from os import sys, path
 sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
 from app.dojo import Dojo
 from app.room import Room
+from app.staff import Staff
+from app.fellow import Fellow
 
 
 class TestRoom(unittest.TestCase):
@@ -17,7 +19,7 @@ class TestRoom(unittest.TestCase):
 		""" tests whether a room was created """
 		TestRoom.reset_total_number_of_rooms()
 		initial_room_count = Room.get_total_number_of_rooms()
-		new_room = Dojo.create_room('Office', ['Orange'])
+		new_room = Dojo.create_room('Office', ['purple'])
 		latter_room_count = Room.get_total_number_of_rooms()
 		self.assertTrue(new_room)
 		self.assertEqual(latter_room_count - initial_room_count, 1)
@@ -27,17 +29,18 @@ class TestRoom(unittest.TestCase):
 		''' test if multiple rooms were successfully created '''
 		TestRoom.reset_total_number_of_rooms()
 		initial_room_count = Room.get_total_number_of_rooms()
-		new_rooms = Dojo.create_room('Office', ['Orange', 'Yellow', 'White'])
+		new_rooms = Dojo.create_room('Office', ['red', 'green', 'blue'])
 		latter_room_count = Room.get_total_number_of_rooms()
 		self.assertTrue(new_rooms)
 		self.assertEqual(latter_room_count - initial_room_count, 3)
 		self.assertNotEqual(initial_room_count, latter_room_count)
 
 	def test_create_rooms_with_valid_name_format(self):
-		""" test if only rooms with valid name format are created """
+		""" test if only rooms with valid names format only are created """
 		TestRoom.reset_total_number_of_rooms()
 		initial_room_count = Room.get_total_number_of_rooms()
-		new_rooms = Dojo.create_room('LivingSpace', ['Orange', 'Yellow', "\"\""])
+		new_rooms = Dojo.create_room(
+					'LivingSpace', ['Orange', 'Yellow', "\"\""])
 		latter_room_count = Room.get_total_number_of_rooms()
 		self.assertTrue(new_rooms)
 		self.assertNotEqual(initial_room_count, latter_room_count)
@@ -61,6 +64,20 @@ class TestRoom(unittest.TestCase):
 	# 	Dojo.remove_room(new_room1)
 	# 	latter_room_count = Dojo.get_total_rooms()
 	# 	self.assertEqual(latter_room_count, 0)
+
+	# def test_add_person(self):
+	# 	""" test  if a person is successfully added to an office """
+	# 	new_office = Dojo.create_room('Office', ['Orange'])
+	# 	self.assertTrue(new_office)
+	# 	new_staff = Staff("Koya", "Gabriel")
+	# 	self.assertTrue(new_staff)
+	# 	Dojo.add_person(new_staff)
+	# 	self.assertEqual(len(new_office.room_members), 1)
+	# 	new_staff2 = Staff("John", "Doe")
+	# 	self.assertTrue(new_staff2)
+	# 	self.assertEqual(len(new_office.room_members), 2)
+
+
 
 
 
