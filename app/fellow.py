dc64b6fa1e5e87dc2d5c9a122a349f6b9a387704
koyexes <no-reply@email.com>
2017-02-14 21:52:42 +0100

    Implements Fellow Class Implements the logic for adding to fellow list. Implements the logic for adding unallocated fellow to list. Implements the logic to get unallocated fellows.

--- app/fellow.py
+++ app/fellow.py
@@ -1,10 +1,35 @@
 from os import sys, path
 sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
 from app.person import Person 
+
 class Fellow(Person):
 	"""docstring for Fellow"""
+
+	fellow_list = []
+	unallocated_fellows = {"office" : [], "livingspace" : []}
+	
 	def __init__(self, lname, fname):
 		super(Fellow, self).__init__(lname, fname)
 		self.category = "fellow"
+		self.livingspace = ""
 
-		
+	def add_to_fellow_list(fellow):
+		Fellow.fellow_list.append(fellow)
+
+	def add_unallocated_fellow(fellow, office=False, livingspace = False):
+		if office and livingspace:
+			Fellow.unallocated_fellows["office"].append(fellow)
+			Fellow.unallocated_fellows["livingspace"].append(fellow)
+		elif office:
+			Fellow.unallocated_fellows["office"].append(fellow)
+		else:
+			Fellow.unallocated_fellows["livingspace"].append(fellow)
+
+	def get_unallocated_fellows():
+		return Fellow.unallocated_fellows
+
+	def set_assigned_livingspace(self, livingspace):
+		self.livingspace = livingspace
+
+
+	
