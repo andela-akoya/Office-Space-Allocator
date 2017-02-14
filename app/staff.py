9660dc0ebf140f691099ef2a50a60e04d5fd6c32
koyexes <no-reply@email.com>
2017-02-14 21:53:44 +0100

    Implements the staff class logic

--- app/staff.py
+++ app/staff.py
@@ -3,8 +3,23 @@ sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 from app.person import Person 
 class Staff(Person):
 	"""docstring for Staff"""
+	staff_list = []
+	unallocated_staff = []
+
 	def __init__(self, lname, fname):
 		super(Staff, self).__init__(lname, fname)
 		self.category = "staff"
+
+	def add_to_staff_list(staff):
+		Staff.staff_list.append(staff)
+
+	def add_unallocated_staff(staff):
+		Staff.unallocated_staff.append(staff)
+
+	def get_unallocated_staff():
+		return Staff.unallocated_staff
+
+	
+		
 		
 		
