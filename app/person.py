819a2d3c0d4c78c9a782496baa7e55a68f573a14
koyexes <no-reply@email.com>
2017-02-14 21:42:05 +0100

    Person class Implementation Implements the assign office logic. Implements the get unallocated logic. Implements the logic that checks the existence of a person

--- app/person.py
+++ app/person.py
@@ -1,5 +1,62 @@
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+from app.utilities import Utilities
+
 class Person():
 	"""docstring for Person"""
+
+	id_map = {}
+
 	def __init__(self, lname, fname):
-		self.surname = lname
-		self.firstname = fname		
+
+		if Utilities.check_format_validity([lname, fname]):
+			self.surname = lname
+			self.firstname = fname
+			self.office = ""
+
+		else:
+			raise Exception
+
+	def set_assigned_office(self, office):
+		self.office = office
+
+	def get_unallocated(unallocated_staff_list, unallocated_fellow_list):
+		output = []
+		output.append("Unallocated List \n---------------------\n")
+		serial_no = 0
+		for staff in unallocated_staff_list:
+			serial_no += 1
+			output.append("{}. Staff {s.surname} {s.firstname}\n" \
+							.format(serial_no, s=staff))
+
+		for fellow in unallocated_fellow_list["office"]:
+			serial_no += 1
+			if fellow in unallocated_fellow_list["livingspace"]:
+				output.append("{}. Fellow {f.surname} {f.firstname}" \
+								+" (Office $ Livingspace)\n" \
+								.format(serial_no, f=fellow))
+			else:
+				output.append("{}. Fellow {f.surname} {f.firstname} " \
+								+"(Office)\n" \
+								.format(serial_no, f=fellow))
+
+		for fellow in unallocated_fellow_list["livingspace"]:
+			
+			if not fellow in unallocated_fellow_list["office"]:
+				serial_no += 1
+				output.append("{}. Fellow {f.surname} {f.firstname} " \ 
+								+" (Livingspace)\n" \
+								.format(serial_no, f=fellow))
+
+		return "\n".join(output)
+
+	def add_to_map(person):
+		Person.id_map[Utilities.generate_person_id(Person.get_id_list())] = person
+		
+	def get_id_list():
+		return list(Person.id_map.keys())
+
+	def exist(person_identifier):
+		if not person_identifier in Person.get_id_list():
+			raise Exception("Person doesn't exist")
+		
