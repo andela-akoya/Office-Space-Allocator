6dea792c33ca5447cc6d3829b84687ca0b17ed47
koyexes <no-reply@email.com>
2017-02-14 21:17:24 +0100

    Livingspace class Implements the add to livingspace list logic Implements the logic to check the existence of an livingspace

--- app/livingspace.py
+++ app/livingspace.py
@@ -4,14 +4,25 @@ from app.room import Room
 
 class LivingSpace(Room):
 	"""docstring for LivingSpace"""
+
 	livingspace_list = {}
 
 	def __init__(self, name):
 		super(LivingSpace, self).__init__(name)
 		self.type = "livingspace"
+		self.maximum_capacity = 6
 
 
-	def add_to_livingspace_list(livingspace_instance):
-		pass
+	def add_to_livingspace_list(livingspace):
+		LivingSpace.livingspace_list[livingspace.name] = livingspace
+	
+
+	def exist(livingspace_name):
+		if livingspace_name in list(LivingSpace.livingspace_list.keys()):
+			raise Exception("A livingspace with the name " \
+							+ livingspace_name  \
+							+ " already exist")
+
+		return True
 	
 		
